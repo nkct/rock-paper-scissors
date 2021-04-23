8e5e25318933dfaf178d414d16dbd132d6a189fe
Miłosz Żegota <83014359+nkct@users.noreply.github.com>
2021-04-23 15:42:49 +0200

    Update rps.py
    Debugged it and updated some logic.

--- rps.py
+++ rps.py
@@ -17,16 +17,16 @@ while True:
 
 	if player_input == 'help':
 		print('\n Commands avialble:\n [scissors] or [s] to play scissors \n [rock] or [r] to play rock \n [paper] or [p] to play paper \n \n [stats] to display streak, sum of wins and w/l ratio\n ')
-		continue
 	
 	elif player_input == 'stats':
+		print("Wins: ", wins)
+		print("Loses: ", loses)
 		print('Streak:', streak)
 		print('Sum of wins:', sum)
 		try:
 			print('Win ratio:', wins/loses)
 		except ZeroDivisionError:
 			print('You need to play a few games before displaying Win Ratio.')
-		continue
 	
 	elif player_input == 'scissors':
 		player = 1
@@ -54,13 +54,12 @@ while True:
 	
 	if enemy == 3:
 		print('Your enemy choose Paper.')
-	
-	if player == enemy:
-		print('Tie!')
-		continue
 
-	if player == 3 and enemy == 1:
+	def lost():
 		print('You Lost!')
+		global sum
+		global loses
+		global streak
 		sum -= 1
 		loses += 1
 		if streak >= 0:
@@ -68,8 +67,12 @@ while True:
 			streak -= 1
 		else:
 			streak -= 1
-	elif player > enemy and player != 1:
+
+	def won():
 		print('You Won!')
+		global sum
+		global wins
+		global streak
 		sum += 1
 		wins += 1
 		if streak <= 0:
@@ -77,21 +80,25 @@ while True:
 			streak += 1
 		else:
 			streak += 1
-	elif enemy == 3:
-			print('You Won!')
-			sum += 1
-			wins += 1
-			if streak <= 0:
-				streak = 0
-				streak += 1
-			else:
-				streak += 1
-	else:
-		print('You Lost!')
-		sum -= 1
-		loses += 1
-		if streak > 1:
-			streak = 0
-			streak -= 1
-		else:
-			streak -= 1
+	
+	if player == enemy:
+		print("Tie!")
+		continue
+
+	if player == 1:
+		if enemy == 2:
+			lost()
+		if enemy == 3:
+			won()
+
+	if player == 2:
+		if enemy == 1:
+			won()
+		if enemy == 3:
+			lost()
+
+	if player == 3:
+		if enemy == 1:
+			lost()
+		if enemy == 2:
+			won()
